0a9c7b1cbce851f68881454280653bc3faa28c12
andrey-gromov <agromov@sfu.ca>
2018-01-28 13:17:25 -0800

    added tests to check return status codes

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,20 +1,34 @@
+import pytest
+from http import HTTPStatus
 from network import server
 from unittest.mock import Mock
-# We want to test if what do_GET function does meets our expectations
-# For example, if a directory listing is requested, how does do_GET respond?
 
-# Tests for which you need to substitute 'self' varaible 
-# with a custom one
-# for example, self.path is modified to a custom filepath
-# and at the end the function that makes a full path is
-# tested for whether it correctly formed the path using self.path
+# create a mock HTTP request handler object to replicate a request from a client
+@pytest.fixture(scope="module")
+def http_request_handler():
+    return Mock(spec=server.ServerRequestHandler)
 
-class TestServer:
-    # don't write the __init__ method
-    
-    def setup(self):
-        self.mock_http_request_handler = Mock(spec=server.ServerRequestHandler)
+# group related tests in a class
+# don't write the __init__ method for classes
 
-    def test_do_get_response_code_200(self):
-        server.ServerRequestHandler.do_GET(self.mock_http_request_handler)
-        self.mock_http_request_handler.send_response.assert_called_with(200)
+class TestServerReturnStatusCodes:
+    @pytest.fixture(params=["/index.xml"])
+    def good_path(self, request):
+        http_request_handler.path = request.param
+
+    @pytest.mark.usefixtures("good_path")
+    def test_do_get_good_path_return_OK(self, http_request_handler):
+        server.ServerRequestHandler.do_GET(http_request_handler)
+        http_request_handler.send_response.assert_called_with(HTTPStatus.OK)
+        http_request_handler.send_error.assert_not_called() # error was not sent
+
+    @pytest.fixture(params=["foo.txt", "index.xml",
+                            "/index.xml/"])
+    def bad_path(self, request):
+        http_request_handler.path = request.param
+
+    @pytest.mark.usefixtures("bad_path")
+    def test_do_get_bad_path_return_NOT_FOUND(self, http_request_handler):
+        server.ServerRequestHandler.do_GET(http_request_handler)
+        http_request_handler.send_error.assert_called_with(HTTPStatus.NOT_FOUND)
+        http_request_handler.send_response.assert_not_called() # good response was not sent
