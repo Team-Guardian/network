85491a99f89bf6ecbb4c4d21d0d74baeaf8d248a
Andrey <agromov@sfu.ca>
2018-02-03 16:31:08 -0800

    Updated server tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -12,8 +12,8 @@ def http_request_handler():
 # don't write the __init__ method for classes
 
 class TestServerReturnStatusCodes:
-    @pytest.fixture(params=["/index.xml"])
-    def good_path(self, request):
+    @pytest.fixture(params=["/", "/images"])
+    def good_path(self, http_request_handler, request):
         http_request_handler.path = request.param
 
     @pytest.mark.usefixtures("good_path")
@@ -22,13 +22,11 @@ class TestServerReturnStatusCodes:
         http_request_handler.send_response.assert_called_with(HTTPStatus.OK)
         http_request_handler.send_error.assert_not_called() # error was not sent
 
-    @pytest.fixture(params=["foo.txt", "index.xml",
-                            "/index.xml/"])
-    def bad_path(self, request):
+    @pytest.fixture(params=["must_fail_test.txt", "/must_fail_test"])
+    def bad_path(self, http_request_handler, request):
         http_request_handler.path = request.param
 
     @pytest.mark.usefixtures("bad_path")
     def test_do_get_bad_path_return_NOT_FOUND(self, http_request_handler):
         server.ServerRequestHandler.do_GET(http_request_handler)
         http_request_handler.send_error.assert_called_with(HTTPStatus.NOT_FOUND)
-        http_request_handler.send_response.assert_not_called() # good response was not sent
