07296cae8ef971d4aa14c42fde259bbc4135c03f
JamesE <jamese@sfu.ca>
2018-02-04 18:56:18 -0800

    Added logging and set client to continuously update
    print() statements are replaced with logger statements which display in the console and are saved to a log which grows to a max of 1MB before cycling. On (temporary?) client side, will run in a continuous loop updating index file, or upon error will attempt to reconnect.

--- client.py
+++ client.py
@@ -1,11 +1,38 @@
 import http.client
 import sys
+import logging
+from time import sleep
+from logging.handlers import RotatingFileHandler
 
-server_ip = sys.argv[1]
-#server_ip = "127.0.1.10"
-x = http.client.HTTPConnection(server_ip, 9999)
-x.connect()
-x.request('GET', '/img/imgs-listen.txt')
-y = x.getresponse()
-z = y.read()
-print(z)
+# setup logger and log format
+logging.basicConfig(level=logging.DEBUG, format = '[%(levelname)s] %(asctime)s | %(module)s | %(message)s')
+logger = logging.getLogger(__name__)
+
+# setup file handler to write logs to a file
+debughandler = RotatingFileHandler('client_log.log', maxBytes = 1048576, backupCount = 1)
+debughandler.setLevel(logging.DEBUG) #set debug handler level to debug
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s | %(module)s | %(message)s')
+debughandler.setFormatter(formatter)
+
+#adds file handler to logger
+logger.addHandler(debughandler)
+
+while True:
+    #server_ip = sys.argv[1]
+    try:
+        server_ip = "127.0.1.20"
+        logger.debug('Target server IP: %s',server_ip)
+        x = http.client.HTTPConnection(server_ip, 9999)
+        x.connect()
+        x.request('GET', '/imgs/imgs-listen.txt')
+
+        y = x.getresponse()
+        z = y.read()
+        logger.info('GET Response received: %s',z)
+        sleep(0.5)
+    except ConnectionRefusedError:
+        logger.error('Connection refused! Server may be offline. Retrying in 5s...')
+        sleep(5)
+    except ConnectionResetError:
+        logger.error('Connected reset! Server may have stopped. Retrying in 5s...')
+        sleep(5)
