7f1484decd319d056f716f79bcb071b133db20eb
Junaid <jjkhan@sfu.ca>
2018-02-12 05:06:09 -0800

    Improved client side of http to constantly request new images from server

--- client.py
+++ client.py
@@ -1,60 +1,137 @@
 import http.client
 import sys
 import os
+import threading, time
+import queue
+from settings import PORT
 
-server_ip = "localhost"
+class _client_list(object):
 
-#server_ip = sys.argv[1]
-server_connection = http.client.HTTPConnection(server_ip,9999)
-server_connection.connect()
-server_connection.request('GET','/img/')
-server_list = server_connection.getresponse().read()
+        def __init__(self):
+            self.client_list = []
+            self.server_list = []
+            self.waiting_list = queue.Queue()
+            self.port = PORT
+            self.server_list_lock = threading.Lock()
+            self.islocked = False
+            self.is_wait_listed = False
+            self.server_ip = "localhost"
+            self.server_connection = http.client.HTTPConnection(self.server_ip, PORT)
+            self.initial_client_list_created = False
 
-server_list = server_list.splitlines()
+        def request_server(self):
 
-#create a directory list for client
-client_list = []
-for root, dirs, files in os.walk(r'C:/Users/Junaid Khan/Desktop'):
-        for file in files:
-            if file.endswith('.jpg'):
-                client_list.append(file)
-
-print('Original Client list:')
-print(client_list)
-#client_list = open('imgs-listing.txt','r').read()
-
-print('Server Image list:')
-print(server_list)
-
-for item in range(server_list.__len__()):
-    b = server_list[item]
-    b = b.decode("utf-8")
-    server_list[item]=b
+            # server_connection = http.client.HTTPConnection(server_ip, PORT)
+             self.server_connection.connect()
 
 
-print('Decoded list:')
-print(server_list)
+             if (self.islocked == False) and (self.is_wait_listed == False):
+              # Get server directory list
+                  self.server_connection.request('GET', '/img/')
+                  self.server_list = self.server_connection.getresponse().read()
+                  self.server_list_lock.acquire()
+                  print('server list is locked now')
+                  self.server_list = self.server_list.splitlines()
 
-#compare the server and client image list
+                  print('Server Image list:')
+                  print(self.server_list)
 
-for item in server_list:
-    if item in client_list:
-        pass
-    else:
-        server_connection.request('GET', '/img/{}'.format(item))
-        with open("{}".format(item),'wb') as f:
-            f.write(server_connection.getresponse().read())
-        f.close()
+                  # Decoding server list
+                  for item in range(self.server_list.__len__()):
+                      b = self.server_list[item]
+                      b = b.decode("utf-8")
+                      self.server_list[item] = b
 
-del client_list[:]
+                  self.islocked = True
+                  print('Decoded Server list:')
+                  print(self.server_list)
+             elif (self.islocked == True) and (self.is_wait_listed == False):
+                self.server_connection.request('GET', '/img/')
+                self.waiting_list.put(self.server_connection.getresponse().read())
+                self.is_wait_listed = True
+             elif (self.islocked == False) and (self.is_wait_listed == True):
+                 del self.server_list [:]
+                 self.server_list = []
+                 for i in range(self.waiting_list.qsize):
+                      self.server_list.append(self.waiting_list.get())
+                 self.is_wait_listed = False
+                 self.islocked=True
 
-for root, dirs, files in os.walk(r'C:/Users/Junaid Khan/Desktop'):
-        for file in files:
-            if file.endswith('.jpg'):
-                client_list.append(file)
+        def updating_client_list(self):
 
-print('Updated Client list:')
-print(client_list)
+            if self.initial_client_list_created == False:
+
+                # create a directory list for client
+                for root, dirs, files in os.walk(r'C:/Users/Junaid Khan/Desktop'):
+                    for file in files:
+                        if file.endswith('.jpg'):
+                            self.client_list.append(file)
+                    self.initial_client_list_created =True
+
+
+            print('Original Client list:')
+            print(self.client_list)
+
+
+         # compare the server and client image list
+            if (self.islocked == True) and (self.is_wait_listed == False):
+                for item in self.server_list:
+                    if item in self.client_list:
+                        pass
+                    else:
+                        self.server_connection.request('GET', '/img/{}'.format(item))
+                        with open("{}".format(item),'wb') as f:
+                            f.write(self.server_connection.getresponse().read())
+                            print('Added {}'.format(item))
+                        f.close()
+                self.islocked = False
+                self.server_list_lock.release()
+
+            # Append the new images to the client's directory
+                del self.client_list [:]
+
+                for root, dirs, files in os.walk(r'C:/Users/Junaid Khan/Desktop'):
+                    for file in files:
+                         if file.endswith('.jpg'):
+                            self.client_list.append(file)
+
+                print('Updated Client list:')
+                print(self.client_list)
+
+
+
+if __name__ == "__main__":
+
+
+    client_list = _client_list()
+
+    thread1 = threading.Thread(target=client_list.request_server())
+    thread2 = threading.Thread(target=client_list.updating_client_list())
+    thread1.start()
+    thread2.start()
+
+    while True:
+        client_list.request_server()
+        client_list.updating_client_list()
+        print('In here')
+        time.sleep(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
