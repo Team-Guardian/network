c6ef8913346471ed6b3c131f87bf8cb6785e745b
Junaid <jjkhan@sfu.ca>
2018-02-14 14:18:12 -0800

    Final Improved Version of Client - Removed Threading and Queue - Works Fine

--- client.py
+++ client.py
@@ -1,121 +1,84 @@
 import http.client
 import sys
 import os
-import threading, time
-import queue
-from settings import PORT
+import time
+from settings import PORT, CLIENT_DIR
+
 
 class _client_list(object):
 
         def __init__(self):
             self.client_list = []
             self.server_list = []
-            self.waiting_list = queue.Queue()
             self.port = PORT
-            self.server_list_lock = threading.Lock()
-            self.islocked = False
-            self.is_wait_listed = False
-            self.server_ip = "localhost"
+            self.server_ip = "localhost" #sys.argv[1]
             self.server_connection = http.client.HTTPConnection(self.server_ip, PORT)
             self.initial_client_list_created = False
 
         def request_server(self):
 
-            # server_connection = http.client.HTTPConnection(server_ip, PORT)
-             self.server_connection.connect()
+              #Get server directory list
+              self.server_connection.request('GET', '/img/')
+              self.server_list = self.server_connection.getresponse().read()
+              self.server_list = self.server_list.splitlines()
 
+              print('Server Image list:')
+              print(self.server_list)
 
-             if (self.islocked == False) and (self.is_wait_listed == False):
-              # Get server directory list
-                  self.server_connection.request('GET', '/img/')
-                  self.server_list = self.server_connection.getresponse().read()
-                  self.server_list_lock.acquire()
-                  print('server list is locked now')
-                  self.server_list = self.server_list.splitlines()
+              #Decoding server list
+              for item in range(self.server_list.__len__()):
+                  b = self.server_list[item]
+                  b = b.decode("utf-8")
+                  self.server_list[item] = b
 
-                  print('Server Image list:')
-                  print(self.server_list)
-
-                  # Decoding server list
-                  for item in range(self.server_list.__len__()):
-                      b = self.server_list[item]
-                      b = b.decode("utf-8")
-                      self.server_list[item] = b
-
-                  self.islocked = True
-                  print('Decoded Server list:')
-                  print(self.server_list)
-             elif (self.islocked == True) and (self.is_wait_listed == False):
-                self.server_connection.request('GET', '/img/')
-                self.waiting_list.put(self.server_connection.getresponse().read())
-                self.is_wait_listed = True
-             elif (self.islocked == False) and (self.is_wait_listed == True):
-                 del self.server_list [:]
-                 self.server_list = []
-                 for i in range(self.waiting_list.qsize):
-                      self.server_list.append(self.waiting_list.get())
-                 self.is_wait_listed = False
-                 self.islocked=True
+              print('Decoded Server list:')
+              print(self.server_list)
 
         def updating_client_list(self):
 
             if self.initial_client_list_created == False:
 
-                # create a directory list for client
-                for root, dirs, files in os.walk(r'C:/Users/Junaid Khan/Desktop'):
+                #create a directory list for client
+                for root, dirs, files in os.walk(CLIENT_DIR):
                     for file in files:
                         if file.endswith('.jpg'):
                             self.client_list.append(file)
                     self.initial_client_list_created =True
 
-
             print('Original Client list:')
             print(self.client_list)
 
+            #Compare the server and client image list
+            for item in self.server_list:
+                if item in self.client_list:
+                    pass
+                else:
+                    self.server_connection.request('GET', '/img/{}'.format(item))
+                    with open("{}".format(item),'wb') as f:
+                        f.write(self.server_connection.getresponse().read())
+                        print('Added {}'.format(item))
+                    f.close()
 
-         # compare the server and client image list
-            if (self.islocked == True) and (self.is_wait_listed == False):
-                for item in self.server_list:
-                    if item in self.client_list:
-                        pass
-                    else:
-                        self.server_connection.request('GET', '/img/{}'.format(item))
-                        with open("{}".format(item),'wb') as f:
-                            f.write(self.server_connection.getresponse().read())
-                            print('Added {}'.format(item))
-                        f.close()
-                self.islocked = False
-                self.server_list_lock.release()
+           #Append the new images to the client's directory
+            del self.client_list [:]
 
-            # Append the new images to the client's directory
-                del self.client_list [:]
+            for root, dirs, files in os.walk(CLIENT_DIR):
+                for file in files:
+                     if file.endswith('.jpg'):
+                        self.client_list.append(file)
 
-                for root, dirs, files in os.walk(r'C:/Users/Junaid Khan/Desktop'):
-                    for file in files:
-                         if file.endswith('.jpg'):
-                            self.client_list.append(file)
-
-                print('Updated Client list:')
-                print(self.client_list)
-
+            print('Updated Client list:')
+            print(self.client_list)
 
 
 if __name__ == "__main__":
 
-
     client_list = _client_list()
-
-    thread1 = threading.Thread(target=client_list.request_server())
-    thread2 = threading.Thread(target=client_list.updating_client_list())
-    thread1.start()
-    thread2.start()
 
     while True:
         client_list.request_server()
         client_list.updating_client_list()
-        print('In here')
-        time.sleep(10)
-
+        time.sleep(0.2)
 
 
 
---
+++ server.py
@@ -0,0 +1,80 @@
+import http.server
+import os
+import sys
+import ssl
+from http import HTTPStatus
+
+# load custom deployment settings
+from settings import SERVER_BASE_DIR, PORT
+
+content_type_dict = {'.jpg':'image/jpeg', 
+                    '.jpeg':'image/jpeg', 
+                    '.txt':'text/plain'}
+
+class ServerRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    # don't override the __init__() method
+
+    def sendDirectoryList(self):
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        
+        for listing in os.listdir(self.path): 
+            self.wfile.write(listing.encode())
+            self.wfile.write('\n'.encode()) 
+                            # convert str to byte stream and send with separators
+
+    def sendHeaders(self):
+        _, file_ext = os.path.splitext(self.path) # get file extension
+        self.send_header('Content-type', content_type_dict[file_ext])
+        self.end_headers()
+
+    def sendFile(self):
+        with open(self.path, 'rb') as f:
+            self.wfile.write(f.read())
+        f.close()
+
+    def do_HEAD(self): # request not supported
+        self.send_error(HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def do_GET(self):
+        self.path = SERVER_BASE_DIR + self.path # modify the path to serve from root directory
+        
+        if not os.path.exists(self.path): # requested file/directory doesn't exist
+            self.send_error(HTTPStatus.NOT_FOUND)
+            return
+
+        self.send_response(HTTPStatus.OK) # by now we know we're OK
+
+        if os.path.isdir(self.path): # general request for directory listing
+            self.sendDirectoryList()
+            return
+
+        # if path is not a directory, it's a file
+        self.sendHeaders()
+        self.sendFile()
+
+    def do_POST(self):
+        print('Received a POST request')
+
+def run(server_ip):
+    server_addr = (server_ip, PORT) # define network addr of this machine
+
+    httpd = http.server.HTTPServer(server_addr, ServerRequestHandler) # create the server
+    # httpd.socket = ssl.wrap_socket(httpd.socket, 
+    #                                 certfile='server.pem', 
+    #                                 server_side=True)
+                                    # wrap underlying socket in SSL and use server-side behavior
+    
+    print('Server is running...')
+    
+    httpd.serve_forever() # begin listening for events
+
+if __name__ == "__main__":
+
+    # parse arguments for server configuration
+    server_ip = "localhost"
+
+    print('Starting the server at {}:{}, with filesystem root directory set to {}'.format(server_ip, PORT, SERVER_BASE_DIR))
+
+    run(server_ip) # start up the server
