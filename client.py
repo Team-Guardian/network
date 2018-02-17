63c575159d833ca5e20c100a52dd62434ef1c83b
Junaid <jjkhan@sfu.ca>
2018-02-17 14:36:26 -0800

    Adjusted comments

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import time
 from settings import PORT, CLIENT_DIR
 
 
-class _client_list(object):
+class Client(object):
 
         def __init__(self):
             self.client_list = []
@@ -14,36 +14,25 @@ class _client_list(object):
             self.server_ip = "localhost" #sys.argv[1]
             self.server_connection = http.client.HTTPConnection(self.server_ip, PORT)
             self.initial_client_list_created = False
+            for file in os.listdir(CLIENT_DIR):
+                if file.endswith('.jpg'):
+                    self.client_list.append(file)
 
-        def request_server(self):
+
+
+        def requestServer(self):
 
               #Get server directory list
               self.server_connection.request('GET', '/img/')
-              self.server_list = self.server_connection.getresponse().read()
-              self.server_list = self.server_list.splitlines()
+              self.server_list = self.server_connection.getresponse().read().decode("utf-8").splitlines()
 
               print('Server Image list:')
               print(self.server_list)
 
-              #Decoding server list
-              for item in range(self.server_list.__len__()):
-                  b = self.server_list[item]
-                  b = b.decode("utf-8")
-                  self.server_list[item] = b
-
               print('Decoded Server list:')
               print(self.server_list)
 
-        def updating_client_list(self):
-
-            if self.initial_client_list_created == False:
-
-                #create a directory list for client
-                for root, dirs, files in os.walk(CLIENT_DIR):
-                    for file in files:
-                        if file.endswith('.jpg'):
-                            self.client_list.append(file)
-                    self.initial_client_list_created =True
+        def updateClientList(self):
 
             print('Original Client list:')
             print(self.client_list)
@@ -58,14 +47,7 @@ class _client_list(object):
                         f.write(self.server_connection.getresponse().read())
                         print('Added {}'.format(item))
                     f.close()
-
-           #Append the new images to the client's directory
-            del self.client_list [:]
-
-            for root, dirs, files in os.walk(CLIENT_DIR):
-                for file in files:
-                     if file.endswith('.jpg'):
-                        self.client_list.append(file)
+                    self.client_list.append(item)
 
             print('Updated Client list:')
             print(self.client_list)
@@ -73,34 +55,10 @@ class _client_list(object):
 
 if __name__ == "__main__":
 
-    client_list = _client_list()
+    client = Client()
 
     while True:
-        client_list.request_server()
-        client_list.updating_client_list()
+        client.requestServer()
+        client.updateClientList()
         time.sleep(0.2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
